edf711ed43480ecec8fc83363d8d4c4f010185f0
Dovah Viking <DovahViking@hotmail.com>
2022-04-01 11:20:29 +0200

    finished app

--- H5_Mini_Svendeprove_App/H5_Mini_Svendeprove_App/Pages/Home.xaml.cs
+++ H5_Mini_Svendeprove_App/H5_Mini_Svendeprove_App/Pages/Home.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using H5_Mini_Svendeprove_App.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace H5_Mini_Svendeprove_App.Pages
 {
@@ -23,8 +25,9 @@ namespace H5_Mini_Svendeprove_App.Pages
             Label_User.Text = user.name;
         }
 
-        private void on_button_start(object sender, EventArgs e)
+        private async void on_button_start(object sender, EventArgs e)
         {
+            await post_start_game_notification(App.path, user);
             App.Current.MainPage = new NavigationPage(new Start(user));
         }
         private void on_button_leaderboards(object sender, EventArgs e)
@@ -34,6 +37,14 @@ namespace H5_Mini_Svendeprove_App.Pages
         private void on_button_options(object sender, EventArgs e)
         {
             App.Current.MainPage = new NavigationPage(new Options(user));
+        }
+
+        static async Task post_start_game_notification(string path, User user)
+        {
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(user.id), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await App.client.PostAsync($"{path}/user/game/{user.id}", content);
+
+            await Task.CompletedTask;
         }
     }
 }
--- H5_Mini_Svendeprove_App/H5_Mini_Svendeprove_App/Pages/Start.xaml.cs
+++ H5_Mini_Svendeprove_App/H5_Mini_Svendeprove_App/Pages/Start.xaml.cs
@@ -7,15 +7,25 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using H5_Mini_Svendeprove_App.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace H5_Mini_Svendeprove_App.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Start : ContentPage
     {
+        User user;
+
         public Start(User user)
         {
             InitializeComponent();
+            this.user = user;
+        }
+
+        private void on_button_ok(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new Home(user));
         }
     }
 }
